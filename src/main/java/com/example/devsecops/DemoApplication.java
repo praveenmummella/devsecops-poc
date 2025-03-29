40de1e30165058ce42759424c5316bf90fd50c46
praveenmummella <80162116+praveenmummella@users.noreply.github.com>
2025-03-29 13:27:02 +0530

    Update DemoApplication.java

--- src/main/java/com/example/devsecops/DemoApplication.java
+++ src/main/java/com/example/devsecops/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.devsecops;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DemoApplicationTest {
-    @Test
-    void contextLoads() {
-        // Test passes if Spring context loads
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
